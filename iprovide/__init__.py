35befa3b853b57ad608c667ca0353e09a5a6638e
David James McCorrie <djmccorrie@gmail.com>
2020-08-01 05:05:11 +0100

    bump version patch

--- iprovide/__init__.py
+++ iprovide/__init__.py
@@ -1,6 +1,6 @@
 """iprovide is a simple package to provide interface definition in Python
 """
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 # pylint: disable=unused-import
 from iprovide.interface import Interface
